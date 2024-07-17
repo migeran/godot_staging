6c9a41d2ebc554d087022c41981f71ec4d92c4c9
Gergely Kis  <gergely.kis@migeran.com>
2024-07-17 02:29:02 +0200

    LibGodot Feature
    * Based on top of the GDExtensionLoader PR: https://github.com/godotengine/godot/pull/91166
    * Add a new GodotInstance GDCLASS that provides startup and iteration commands to control a Godot instance.
    * Adds a libgodot_create_godot_instance entry point that creates a new Godot instance and returns a GodotInstance object.
    * Adds a libgodot_destroy_godot_instance entry point that destroys the Godot instance.
    * Allow specifying an external native rendering surface handle to render Godot into the UI of a host application.
      * It is also possible to embed multiple Godot windows into the UI of a host application.
      * Currently supported on MacOS and iOS
    
    Sample Apps: https://github.com/migeran/libgodot_project
    
    Developed by [Migeran](https://migeran.com)
    
    Sponsors & Acknowledgements:
    
    * Initial development sponsored by [Smirk Software](https://www.smirk.gg/)
    * Rebasing to Godot 4.3 and further development sponsored by [Xibbon Inc.](https://xibbon.com)
    * The GDExtension registration of the host process & build system changes were based
      on @Faolan-Rad's LibGodot PR: https://github.com/godotengine/godot/pull/72883
    
    Co-Authored-By: Gabor Koncz <gabor.koncz@migeran.com>

--- main/main.cpp
+++ main/main.cpp
@@ -774,6 +774,7 @@ Error Main::test_setup() {
 
 	ClassDB::set_current_api(ClassDB::API_CORE);
 #endif
+	register_core_platform_apis();
 	register_platform_apis();
 
 	// Theme needs modules to be initialized so that sub-resources can be loaded.
@@ -836,6 +837,7 @@ void Main::test_cleanup() {
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	unregister_platform_apis();
+	unregister_core_platform_apis();
 	unregister_driver_types();
 	unregister_scene_types();
 
@@ -846,6 +848,7 @@ void Main::test_cleanup() {
 	GDExtensionManager::get_singleton()->deinitialize_extensions(GDExtension::INITIALIZATION_LEVEL_SERVERS);
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SERVERS);
 	unregister_server_types();
+	unregister_core_server_types();
 
 	EngineDebugger::deinitialize();
 	OS::get_singleton()->finalize();
@@ -949,6 +952,8 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	register_core_types();
 	register_core_driver_types();
+
+	register_core_platform_apis();
 
 	MAIN_PRINT("Main: Initialize Globals");
 
@@ -2617,6 +2622,9 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	message_queue = memnew(MessageQueue);
 
+	// Register Core Server Types
+	register_core_server_types();
+
 	Thread::release_main_thread(); // If setup2() is called from another thread, that one will become main thread, so preventively release this one.
 	set_current_thread_safe_for_nodes(false);
 
@@ -2671,6 +2679,7 @@ error:
 		memdelete(packed_data);
 	}
 
+	unregister_core_platform_apis();
 	unregister_core_driver_types();
 	unregister_core_extensions();
 
@@ -4605,6 +4614,7 @@ void Main::cleanup(bool p_force) {
 	GDExtensionManager::get_singleton()->deinitialize_extensions(GDExtension::INITIALIZATION_LEVEL_SERVERS);
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_SERVERS);
 	unregister_server_types();
+	unregister_core_server_types();
 
 	EngineDebugger::deinitialize();
 
